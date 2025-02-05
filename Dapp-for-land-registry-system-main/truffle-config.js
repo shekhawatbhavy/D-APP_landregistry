fb275091dd3b42b53a1d8a4d06305e94cf93bfc3
shekhawatbhavy <shekhawat.bhavy16@gmail.com>
2025-02-05 16:45:38 +0530

    Update truffle-config.js

--- Dapp-for-land-registry-system-main/truffle-config.js
+++ Dapp-for-land-registry-system-main/truffle-config.js
@@ -12,14 +12,14 @@
  * --------------------------------
  *
  * Do you have a complex application that requires lots of transactions to deploy?
- * Use this approach to make deployment a breeze 🏖️:
+ * Use this approach to make deployment a breeze :
  *
  * Infura deployment needs a wallet provider (like @truffle/hdwallet-provider)
  * to sign transactions before they're sent to a remote public node.
  * Infura accounts are available for free at 🔍: https://infura.io/register
  *
  * You'll need a mnemonic - the twelve word phrase the wallet uses to generate
- * public/private key pairs. You can store your secrets 🤐 in a .env file.
+ * public/private key pairs. You can store your secrets in a .env file.
  * In your project root, run `$ npm install dotenv`.
  * Create .env (which should be .gitignored) and declare your MNEMONIC
  * and Infura PROJECT_ID variables inside.
@@ -31,7 +31,7 @@
  * Deployment with Truffle Dashboard (Recommended for best security practice)
  * --------------------------------------------------------------------------
  *
- * Are you concerned about security and minimizing rekt status 🤔?
+ * Are you concerned about security and minimizing rekt status ?
  * Use this method for best security:
  *
  * Truffle Dashboard lets you review transactions in detail, and leverages
